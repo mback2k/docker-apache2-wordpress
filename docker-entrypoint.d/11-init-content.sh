aefd38688496da1b5e652adf02a762c992515ae0
Marc Hoersken <info@marc-hoersken.de>
2018-05-27 18:10:33 +0200

    Fix relative paths in entrypoint script

--- docker-entrypoint.d/11-init-content.sh
+++ docker-entrypoint.d/11-init-content.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 set -e
 
-cd /var/www/wordpress
-
-if [ ! -d "wp-content" ]; then
-    mkdir wp-content
+if [ ! -d "wordpress/wp-content" ]; then
+    mkdir wordpress/wp-content
 fi
 
-if [ ! -f "wp-content/index.php" ]; then
+if [ ! -f "wordpress/wp-content/index.php" ]; then
     tar xfvz wordpress-content.tar.gz
 fi
 
